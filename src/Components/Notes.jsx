10791de6901c0d9646dea67fcc7ba8c364f1fa6b
Anubrata2000 <109304431+Anubrata2000@users.noreply.github.com>
2022-12-04 12:48:10 +0530

    First Commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DC FUTURE SLATE</h1>
+      <p>-------------------------------------</p>
+      <p>
+        1. Shazam! Fury of the Gods - Release Date: March 17, 2023. Director:
+        David F. Sandberg. 2. The Flash - Release Date: June 23, 2023.
+        Director:Andy Muschietti. 3. Blue Beetle - Release Date: August 18,
+        2023. Director: Angel Manuel Soto. 4. Aquaman and the Lost Kingdom -
+        Release Date: December 25, 2023. Director: James Wan. 5. Joker: Folie À
+        Deux - Release Date: October 4, 2024. Director: Todd Phillips. 6. Wonder
+        Woman 3 - Release Date : TBA. Director: Patty Jenkins. 7.The Batman
+        Sequel - Release Date : TBA. Director: Matt Reeves.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
